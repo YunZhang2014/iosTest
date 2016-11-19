388122d5a95f6844029357c07266649404b62d03
YunZhang2014 <jslgdxzy@gmail.com>
2016-11-19 19:40:36 +1030

    add files

---
+++ objectCTest/objectCGeneral/LearnObjective-ContheMacForOSXandiOSE22012Test/my/Hello Objective-C-My/Hello Objective-C-My/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Objective-C-My
+//
+//  Created by Yun Zhang on 19/11/16.
+//  Copyright © 2016 Yun Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return (0);
+}
