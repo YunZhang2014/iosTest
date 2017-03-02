720c92bd0fcfc096a1cb0a56805a54561e0c5d39
YunZhang2014 <jslgdxzy@gmail.com>
2017-03-02 20:24:43 +1030

    change

---
+++ objectCTest/objectCGeneral/LearnObjective-ContheMacForOSXandiOSE22012Test/my/c02/hello/hello/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  hello
+//
+//  Created by Yun Zhang on 2/3/17.
+//  Copyright © 2017 Yun Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    NSLog(@"Hello,Objective C");
+    return (0);
+}
