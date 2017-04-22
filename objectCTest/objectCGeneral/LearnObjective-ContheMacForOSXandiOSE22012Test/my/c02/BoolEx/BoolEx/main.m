34a5c6c0540a41beadd7f7f1f4ff9555fc84595f
YunZhang2014 <jslgdxzy@gmail.com>
2017-04-22 21:04:26 +0930

    ic

---
+++ objectCTest/objectCGeneral/LearnObjective-ContheMacForOSXandiOSE22012Test/my/c02/BoolEx/BoolEx/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BoolEx
+//
+//  Created by Yun Zhang on 2/3/17.
+//  Copyright © 2017 Yun Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
