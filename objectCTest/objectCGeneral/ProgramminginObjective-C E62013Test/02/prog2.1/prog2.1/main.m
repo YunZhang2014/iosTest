55027ae2013960505e842b2da1f70eaf4b310963
YunZhang2014 <jslgdxzy@gmail.com>
2017-04-27 12:27:06 +0930

    add

---
+++ objectCTest/objectCGeneral/ProgramminginObjective-C E62013Test/02/prog2.1/prog2.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog2.1
+//
+//  Created by Yun Zhang on 27/4/17.
+//  Copyright © 2017 Yun Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
