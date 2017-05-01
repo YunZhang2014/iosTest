558ade4961c10bb150daa1eba54375082b836a32
YunZhang2014 <jslgdxzy@gmail.com>
2017-05-02 08:29:47 +0930

    add files

---
+++ objectCTest/objectCGeneral/Objective-CForDummies2009Test/02/hello/hello/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello
+//
+//  Created by Yun Zhang on 2/5/17.
+//  Copyright © 2017 Yun Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
