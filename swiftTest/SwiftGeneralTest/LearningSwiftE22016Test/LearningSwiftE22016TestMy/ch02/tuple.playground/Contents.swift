217c91bab4faa13ac069f5d9cd207c50b239dad4
YunZhang2014 <jslgdxzy@gmail.com>
2017-05-01 09:56:57 +0930

    add files

---
+++ swiftTest/SwiftGeneralTest/LearningSwiftE22016Test/LearningSwiftE22016TestMy/ch02/tuple.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var card = (color: "Red", suit: "Hearts", value: 7)
+
+card.color
+
+var diceRoll = (4,6)
+
+diceRoll.0
+
+let (first,second) = diceRoll;
+first
+second
+
+diceRoll = (4,5)
+diceRoll.0 = 2
+diceRoll
